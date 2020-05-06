089622dc6eb409bcfccf7418b6f54e9877d64eaa
waheeedkhan00 <64815910+waheeedkhan00@users.noreply.github.com>
2020-05-07 01:31:51 +0500

    Update khanji.py

--- khanji.py
+++ khanji.py
@@ -48,12 +48,12 @@ def jalan(z):
 
 ##### LOGO #####
 logo = """ 
-\033[1;96mWAHEED KHAN PATHAN ZINDABAD
+\033[1;96mWAHEED KHAN PATHAN ZINDABAD 786
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mWaheed Khan
+\033[1;96mAuthor  \033[1;93m: \033[1;97mWaheed Khan
 \033[1;96mInstagram \033[1;93m: \033[1;Waheedkhan00
 \033[1;96mFacebook  \033[1;93m: \033[1; Waheedkhan00
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Waheedkhan00/waheed
+\033[1;96mGithub \033[1;93m: \033[1;98mhttps://github.com/Waheedkhan00/waheed
 \033[1;91m======================================="""
 
 def tik():
